b12c25dc7e7631cac66bcfc59650686bebc4a5ee
Mario Gurmesevski <mariogurmesevski1@gmail.com>
2023-03-14 19:27:51 +0100

    commit

--- Node JS/Class 6 - Recap/server/index.js
+++ Node JS/Class 6 - Recap/server/index.js
@@ -1,1 +1,25 @@
-console.log("raboti 3");
+import express from "express";
+import cors from "cors";
+
+// Creating a server
+const app = express();
+
+// Handling middleware
+// app.use((req, res, next) => {
+//   console.log("Middleware has been executed");
+//   next();
+// });
+app.use(express.json()); //to enable working with JSON in body
+app.use(cors());
+
+// Rotes
+app.use("/api", (req, res) => {
+  console.log("The request has been sent to /api");
+
+  res.sendStatus(200);
+});
+
+// Server listening
+app.listen(3000, "localhost", () => {
+  console.log("The server has started listening on http://localhost:3000");
+});
