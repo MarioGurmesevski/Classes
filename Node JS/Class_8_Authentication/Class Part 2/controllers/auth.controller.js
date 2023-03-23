66dc80af1800c3febb0a671a05371dd2b925e88c
Mario Gurmesevski <mariogurmesevski1@gmail.com>
2023-03-23 19:48:15 +0100

    commit

--- Node JS/Class_8_Authentication/Class Part 2/controllers/auth.controller.js
+++ Node JS/Class_8_Authentication/Class Part 2/controllers/auth.controller.js
@@ -1,21 +1,24 @@
-import AuthModel from "../models/auth.model.js";
+import AuthModel from '../models/auth.model.js'
 
-const authModel = new AuthModel();
+const authModel = new AuthModel()
 
 export default class AuthController {
-  registerUser(req, res) {
-    const userData = req.body;
+    async registerUser(req, res) {
+        const userData = req.body;
+        
+        const newUser = await authModel.registerUser(userData);
 
-    const newUser = authModel.registerUser(userData);
+        res.status(200).send(newUser)
+    }
 
-    res.status(200).send(newUser);
-  }
+    async loginUser(req, res) {
+        const userData = req.body;
 
-  loginUser(req, res) {
-    const userData = req.body;
-
-    authModel.loginUser(userData);
-
-    res.sendStatus(200);
-  }
+        try {
+            const loginData = await authModel.loginUser(userData)
+            res.status(200).send(loginData)
+        } catch (error) {
+            res.status(400).send(error.message)
+        }        
+    }
 }
--- Node JS/Class_8_Authentication/Class Part 2/models/auth.model.js
+++ Node JS/Class_8_Authentication/Class Part 2/models/auth.model.js
@@ -1,39 +1,47 @@
-import bcrypt from "bcryptjs";
-import DataService from "../services/data.service.js";
+import bcrypt from 'bcryptjs';
+import DataService from '../services/data.service.js'
+import { createAccessToken } from '../jwt.const.js'
+import { v4 as uuidv4 } from 'uuid'
 
 export default class AuthModel {
-  registerUser(userData) {
-    const hashedPassword = bcrypt.hashSync(userData.password, 10);
+    async registerUser(userData) {
+        const hashedPassword = await bcrypt.hash(userData.password, 10)
 
-    const user = {
-      id: uuidv4(),
-      username: userData.username,
-      password: hashedPassword,
-    };
+        const user = {
+            id: uuidv4(),
+            username: userData.username,
+            password: hashedPassword
+        }
 
-    DataService.writeFile("./data/users.json", [user]);
+        await DataService.saveFile('./data/users.json', [user])
 
-    return user;
-  }
+        const { password, ...everythingElse } = user;
 
-  loginUser() {
-    const users = DataService.readFile("./data/users.json");
-
-    const user = users.find((user) => user.username === userData.username);
-
-    if (!user) {
-      throw new Error("User not found");
+        return everythingElse;
     }
 
-    const isSamePassword = bcrypt.compareSync(userData.password, user.password);
+    async loginUser(userData) {
+        const users = await DataService.readFile('./data/users.json')
 
-    if (isSamePassword) {
-      const token = createAccessToken(user.id);
-      console.log(token);
-      const { password, ...whatIsLeftOfUser } = user;
-      return whatIsLeftOfUser;
-    } else {
-      throw new Error("Invalid credentials");
+        console.log(users)
+    
+        const user = users.find(user => user.username === userData.username)
+    
+        console.log('user', user)
+        if (!user) {
+            throw new Error('User not found')
+        }
+    
+        const isSamePassword = await bcrypt.compare(userData.password, user.password)
+    
+        console.log('is same pass', isSamePassword)
+        if (isSamePassword) {
+            const token = createAccessToken(user.id)
+            console.log('token', token)
+            const { password, ...whatIsLeftOfUser } = user;
+            return { user: whatIsLeftOfUser, token };
+        } else {
+            throw new Error('Invalid credentials')
+        }
     }
-  }
 }
