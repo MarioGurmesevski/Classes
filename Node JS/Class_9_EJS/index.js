50ea247f5d12390815cb081bd0e505c5df704764
mariogurmesevski <mariogurmesevski1@gmail.com>
2023-03-24 19:36:43 +0100

    commit

--- Node JS/Class_9_EJS/index.js
+++ Node JS/Class_9_EJS/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import cors from "cors";
 import router from "./router.const.js";
 
@@ -8,6 +8,8 @@ const PORT = 3000;
 const HOSTNAME = "localhost";
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.set("view engine","ejs")
 
