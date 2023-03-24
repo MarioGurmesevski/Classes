8f1ce6faf2fa7b2378e5dd1c0918e2db8172fb60
mariogurmesevski <mariogurmesevski1@gmail.com>
2023-03-24 18:37:40 +0100

    commit

--- Node JS/Class_9_EJS/index.js
+++ Node JS/Class_9_EJS/index.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import cors from 'cors';
-import router from './router.const.js';
+import express from "express";
+import cors from "cors";
+import router from "./router.const.js";
 
-const app = express()
+const app = express();
 
 const PORT = 3000;
-const HOSTNAME = 'localhost';
+const HOSTNAME = "localhost";
 
-app.use(cors())
+app.use(cors());
 
-app.set('view engine', 'ejs');
+app.set("view engine","ejs")
 
-app.use(router)
+app.use(router);
 
 app.listen(PORT, HOSTNAME, () => {
-    console.log(`Server started listening on http://${HOSTNAME}:${PORT}`)
-})
+  console.log(`Server start listening on http://${HOSTNAME}:${PORT}`);
+});
