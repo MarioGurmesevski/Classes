c97a62f4ce6dd18d83037a09deac020d9934a5bc
Mario Gurmesevski <mariogurmesevski1@gmail.com>
2023-05-18 17:27:32 +0200

    c

--- Database Development & Design/Class_1_Intro/class1.sql
+++ Database Development & Design/Class_1_Intro/class1.sql
@@ -1,32 +1,45 @@
 -- DDL statements
+
 CREATE TABLE IF NOT EXISTS test ();
 
-Create TABLE users ();
+CREATE TABLE users ();
 
-DROP TABLE IF EXISTS test ();
-DROP TABLE users()
+DROP TABLE IF EXISTS users;
 
--- 1. Name of the column
+DROP TABLE IF EXISTS test;
+
+-- 1. Name of the column 
 -- 2. Data type of the column
--- 3. Option on columns
+-- 3. Options on columns
 
-CREATE TABLE BuisnessEntity(
+CREATE TABLE BusinessEntity (
 	Id INTEGER PRIMARY KEY,
 	Name varchar(100),
 	Region varchar(1000) NULL,
 	ZipCode char(4) NULL,
-	Size varchar(10) NULL,
+	Size varchar(10) NULL
 )
 
-INSERT INTO BuisnessEntity (Id,Name,Region,ZipCode,Size)
-VALUES(1,'Tinex','Skopje','1000','large')
+INSERT INTO BusinessEntity (Id, Name, Region, ZipCode, Size)
+VALUES (1, 'Tinex', 'Skopje', '1000', 'large')
 
-INSERT INTO BuisnessEntity (Id,Name,Region,ZipCode,Size)
-VALUES(2,'Ramstor','Skopje','1000','medium')
+INSERT INTO BusinessEntity (Id, Name, Region, ZipCode, Size)
+VALUES (2, 'Vero', 'Bitola', '2000', 'medium')
 
-SELECT * FROM BuisnessEntity
+-- we only need to add NOT NULL values
+INSERT INTO BusinessEntity (Name, id)
+VALUES ('Zito', 3)
+
+-- THIS IS INVALID! Be careful on the ordering od the parameters!
+INSERT INTO BusinessEntity (Name, id)
+VALUES ('Zito', 3)
+
+SELECT * FROM BusinessEntity
 
 -- varchar in SQL === string in JS
+
+-- NULL = OPTIONAL
+-- NOT NULL = REQUIRED
 
 CREATE TABLE Employee
 (
@@ -41,62 +54,57 @@ CREATE TABLE Employee
 
 DROP TABLE Employee
 
--- DATE FORMAT YEAR/DAY/MONTH
+-- DATE FORMAT YEAR/DATE/MONTH
 
-INSERT INTO Employee(Id,FirstName,LastName,DateOfBirth,Gender,HireDate,NationalIdNumber)
-VALUES (1,'Mario','Gurmesevski','09/07/2005','M','10/10/2020','21243151')
+INSERT INTO Employee (Id, FirstName, LastName, DateOfBirth, Gender, HireDate, NationalIdNumber)
+VALUES (1, 'Nikola', 'Stojkovski', '07/08/2003', 'M', '10/05/2023', '1234569')
 
-INSERT INTO Employee(Id,FirstName,LastName,DateOfBirth,Gender,HireDate,NationalIdNumber)
-VALUES (2,'John','Doe','2002-07-09','M','10/10/2020','124124235')
+INSERT INTO Employee (Id, FirstName, LastName, DateOfBirth, Gender, HireDate, NationalIdNumber)
+VALUES (2, 'Todor', 'Pelivanov', '2003-02-01', 'M', '11/05/2023', '123456931')
 
-SELECT * FROM Employee
-
-SELECT Id,FirstName,LastName,DateOfBirth FROM Employee
 
 SELECT * FROM Employee
-WHERE FirstName = 'John'
+
+
+SELECT Id, FirstName, LastName, DateOfBirth FROM Employee
+
+SELECT * FROM Employee
+WHERE FirstName = 'Todor'
 
 -- UPDATE STATEMENTS
 
 UPDATE Employee
 SET DateOfBirth = '05/02/2000'
-WHERE Id = 2
+WHERE ID = 2
+
 
 -- DELETE STATEMENTS
 
-DELETE 
+DELETE
 FROM Employee
-WHERE Id = 1
+WHERE ID = 1
 
--- Customer 
+-- Customer
 -- id
 -- Name - string
--- AccountNumber - number
+-- AccountNmber - number
 -- City - string
--- RegionName - string
--- PhoneNumber - string
--- isActive - boolean
+-- RegionName -- string
+-- PhoneNumber -- string
+-- isActive -- boolean
 
-CREATE TABLE Customer
+CREATE TABLE Customer 
 (
 	Id INTEGER PRIMARY KEY,
 	Name varchar(50),
 	AccountNumber INTEGER,
 	City varchar(50),
 	RegionName varchar(50),
-	PhoneNumber varchar(50),
-	isActive boolean 
+	PhoneNumber varchar(50), -- +389(0)21236978
+	isActive boolean
 )
 
-SELECT * FROM Customer
+select * from Customer
 
-INSERT INTO Customer (Id,Name,AccountNumber,City,RegionName,PhoneNumber,isActive)
-VALUES (1,'John',42141,'USA','USA','4214124',true)
-
-
-
-
-
-
-
-
+INSERT INTO Customer (Id, name, accountnumber, city, regionname, phonenumber, isactive)
+VALUES (3, 'Ivan', 1, 'Kumanovo', 'Kumanovo', '+8673125361', '1')
