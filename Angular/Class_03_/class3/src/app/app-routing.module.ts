37bdb967a551e46924df7403fd7c624b8aa1acbf
Mario Gurmesevski <mariogurmesevski1@gmail.com>
2023-08-12 12:19:47 +0200

    Finished Class

--- Angular/Class_03_/class3/src/app/app-routing.module.ts
+++ Angular/Class_03_/class3/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
+import { StudentListComponent } from './components/students-list/student-list/student-list.component';
+import { NotFoundComponent } from './components/not-found/not-found/not-found.component';
+import { StudentDetailsComponent } from './components/student-details/student-details/student-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'students', component: StudentListComponent },
+  { path: 'students/:id', component: StudentDetailsComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Angular/Class_03_/class3/src/app/app.module.ts
+++ Angular/Class_03_/class3/src/app/app.module.ts
@@ -4,9 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
+import { StudentListComponent } from './components/students-list/student-list/student-list.component';
+import { AverageGradePipe } from './pipes/average-grade.pipe';
+import { GradeColorDirective } from './directives/grade-color.directive';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found/not-found.component';
+import { TopSectionComponent } from './components/top-section/top-section/top-section.component';
+import { StudentDetailsComponent } from './components/student-details/student-details/student-details.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    StudentListComponent,
+    AverageGradePipe,
+    GradeColorDirective,
+    NavbarComponent,
+    NotFoundComponent,
+    TopSectionComponent,
+    StudentDetailsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
