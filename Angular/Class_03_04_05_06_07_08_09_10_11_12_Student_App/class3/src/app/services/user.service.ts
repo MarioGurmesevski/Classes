c848dd7360f5a51f0b9847a6bfa5c9d25cc04523
Mario Gurmesevski <mariogurmesevski1@gmail.com>
2023-09-12 20:20:40 +0200

    end of class

--- Angular/Class_03_04_05_06_07_08_09_10_11_12_Student_App/class3/src/app/services/user.service.ts
+++ Angular/Class_03_04_05_06_07_08_09_10_11_12_Student_App/class3/src/app/services/user.service.ts
@@ -1,13 +1,17 @@
+import { NotificationsService } from './notifications.service';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
-import { User } from '../interfaces/user.interface';
+import { Roles, User } from '../interfaces/user.interface';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private firestore: AngularFirestore) {}
+  constructor(
+    private firestore: AngularFirestore,
+    private notificationsService: NotificationsService
+  ) {}
 
   searchUsers(userName: string): Observable<User[]> {
     return this.firestore
@@ -20,5 +24,21 @@ export class UserService {
           )
         )
       );
+  }
+  async updateUserRoles(id: string, roles: Roles) {
+    const userRef = this.firestore.collection<User>('users').doc(id);
+
+    try {
+      await userRef.update({ roles });
+      this.notificationsService.pushNotification(
+        'User role updated successfully',
+        'success'
+      );
+    } catch (error: any) {
+      this.notificationsService.pushNotification(
+        error.message || 'Error while updated user roles',
+        'error'
+      );
+    }
   }
 }
